c5eafa03f0bfed4518ee1cf937bba7d35859c43d
teodora88 <nikolic8teodora@gmail.com>
2023-05-22 23:52:19 +0200

    Kreiran Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar(){
+    return (
+    <div className="inner-width">  
+        <nav className="navigation-menu">
+          <h1 className='title'>TechWorld</h1>
+          <a href="#"> Pocetna</a>
+          <a href="#"> Proizvodi</a>
+          <a href="#"> Omiljeno</a>
+        </nav>
+      </div>
+
+    )
+}
+
+export default Navbar
